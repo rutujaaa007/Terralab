7f2945018294f0e0a55ed6221d2eee51ec6f70e0
rutujaaa007 <rutujashivpuje7@gmail.com>
2025-08-21 09:51:08 +0530

    version.tf

--- version.tf
+++ version.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "rutuja-2025"
+    key            = "terraform.tfstate"
+    region         = "eu-west-3"
+    encrypt        = true
+    use_lockfile   = true 
+  }
+
+}
 
