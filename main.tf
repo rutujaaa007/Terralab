8b78ce96403cd028a15f9a6ba50f4c99967b1ac8
rutujaaa007 <rutujashivpuje7@gmail.com>
2025-08-21 09:23:34 +0530

    file added

--- main.tf
+++ main.tf
@@ -1,23 +1,53 @@
-# provider "aws" {
-  # region = "eu-west-3"
-  
-# }
-#  
-# resource "aws_instance" "my_ec2" {
-  # ami = "ami-04ec97dc75ac850b1"
-  # instance_type = "t3.micro"
-#  
-  # tags = {
-    # Name = "terraform_Rutuja"
-  # }
-# }
+resource "aws_security_group" "ec2_sg" {
+  name        = "rutuja-security"
+  description = "Allow SSH, HTTP, and custom 8080"
 
-resource "aws_s3_bucket" "rutuja_bucket" {
-  bucket = "rutuja-2025"
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow custom app port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-04ec97dc75ac850b1" 
+  instance_type = "t2.micro"
+  key_name      = "rutujk" 
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-  Name        = "Rutuja-Bucket"
-  Environment = "Development"
-}
+    Name = "Terraform-EC2-Lab-Ubuntu"
+  }
 }
 
+output "instance_public_ip" {
+  description = "The public ip address of the ec2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
