cfa2be27a50d843ce42bf43f0b9589f02e5f151a
rutujaaa007 <rutujashivpuje7@gmail.com>
2025-08-21 09:58:53 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -47,9 +47,4 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-#output "instance_public_ip" {
-  #description = "The public ip address of the ec2 instance"
-  #value       = aws_instance.my_ec2.public_ip
-
-}#
 
