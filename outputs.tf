a33abe4ef155f5569cec38a1241e4752f4a3dedf
rutujaaa007 <rutujashivpuje7@gmail.com>
2025-08-21 09:59:40 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,13 @@
-
-output "s3_bucket_id" {
-  description = "The name (ID) of the S3 bucket."
-  value       = aws_s3_bucket.rutuja_bucket.id
+output "instance_public_ip" {
+  description = "The public ip address of the ec2 instance"
+  value       = aws_instance.my_ec2.public_ip
 
 }
+
+
+#output "s3_bucket_id" {
+ # description = "The name (ID) of the S3 bucket."
+  #value       = aws_s3_bucket.rutuja_bucket.id
+
+#}
+
